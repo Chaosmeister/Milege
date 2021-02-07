d627f57bd1a86ea323698586b1922db740edda36
Tomas Dittmann <chaosmeist3r@gmail.com>
2021-02-07 11:41:08 +0100

    connect story assembly

--- story.js
+++ story.js
@@ -1,51 +1,75 @@
 document.getElementById('input').addEventListener("input", createStory);
 
 function createStory() {
-    var base64images = [];// convert(document.getElementById('pictures').files);
+    var base64images = collectImages();
     createInlineImages(base64images);
     assembleStory(document.getElementById('input').textContent, base64images);
 };
 
-function convert(files) {
-    var base64images = [];
-    for (let i = 0; i < files.length; ++i) {
-        const file = files[i];
+function collectImages() {
+    var images = [];
 
-        if (!file.type.startsWith('image/')) { continue }
+    Array.from(document.getElementsByClassName("image")).forEach(function (element) {
+        var image = extractData(element);
+        var pills = extractWords(element);
 
-        base64images.push({ name: file.name.replace(/\.[^/.]+$/, ""), data: toBase64(file) });
-    }
-    return base64images;
+        images.push({ names: pills, data: image });
+    })
+
+    return images;
 }
 
-const toBase64 = file => new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => resolve(reader.result);
-    reader.onerror = error => reject(error);
-});
+function extractData(element) {
+    var Preview = element.childNodes.item(1);
+    var Image = Preview.childNodes.item(1);
 
-async function createInlineImages(base64images) {
+    return Image.src;
+}
+
+function extractWords(element) {
+    var words = [];
+
+    Array.from(element.childNodes.item(3).childNodes).forEach(function (pill) {
+        if (pill.innerText) {
+            words.push(pill.innerText);
+        }
+    });
+
+    words.pop();
+
+    return words;
+}
+
+function createInlineImages(base64images) {
     var style = "<style>";
 
-    for (let i = 0; i < base64images.length; ++i) {
-        const image = base64images[i];
-        style += "\n ." + image.name + "{background: url(" + await image.data + ") no-repeat left center;background-size: 100% 100%;}";
-    }
+    base64images.forEach(function (image) {
+        var names = "";
+        image.names.forEach(function (name) {
+            names += "." + name + ", ";
+        })
+
+        names = names.slice(0,-2);
+
+        style += "\n " + names + "{background: url(" + image.data + ") no-repeat left center;background-size: 100% 100%;}";
+    });
 
     style += "\n</style>";
 
-    document.getElementById('preview').innerHTML = style + document.getElementById('preview').innerHTML;
+    document.getElementById('result').innerHTML = style;
 }
 
-async function assembleStory(text, images) {
+function assembleStory(text, images) {
     for (let i = 0; i < images.length; ++i) {
         const image = images[i];
-        text = text.replaceAll(new RegExp("\\b" + image.name + "\\b", 'g'), "<div class=\"image " + image.name + "\"></div>");
+        
+        image.names.forEach(function (name) {
+            text = text.replaceAll(new RegExp("\\b" + name + "\\b", 'g'), "<div class=\"inlineimage " + name + "\"></div>");
+        })
     }
 
     text = text.replace(/(?:\r\n|\r|\n)/g, '</span><span>');
     text = text.replaceAll("</span><span></span><span>", '</span><span>');
 
-    document.getElementById('result').innerHTML = "<span>" + text + "</span>";
+    document.getElementById('result').innerHTML += "<span>" + text + "</span>";
 }
